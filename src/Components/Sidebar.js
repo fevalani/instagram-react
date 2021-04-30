5a6062cada95da6f45edf1b1440a1da2d767a5d8
Fernando Valani Cavalcante <valanife@gmail.com>
2021-04-30 18:23:39 -0300

    Corpo componetization

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,13 @@
+export default function Sidebar() {
+    return (
+        <div class="sidebar">
+            <div class="usuario">
+                <img src="assets/img/catanacomics.svg" />
+                <div class="texto">
+                <strong>catanacomics</strong>
+                Catana
+                </div>
+            </div>
+        </div>
+    );
+}
